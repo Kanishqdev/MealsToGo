da08510cdd499798abaced0d602435aea560ba2a
Kanishqdev <kanishq.k@gmail.com>
2022-11-13 14:13:48 +0530

    Basic changes: Code formatter, Basic classes

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { Search } from "./src/features/Search";
+import React from "react";
+import { RestuarantList } from "./src/features/RestuarantList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Search style={styles.search} />
+        <RestuarantList style={styles.list} />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "orange",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "red",
   },
 });
---
+++ src/features/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Search = ({ style }) => {
+  return (
+    <View style={style}>
+      <Text>Search</Text>
+    </View>
+  );
+};
