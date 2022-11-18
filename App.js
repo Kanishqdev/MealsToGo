ceba0d995a73e2d44c8435270e6d1fc3ed5b5594
Kanishqdev <kanishq.k@gmail.com>
2022-11-18 22:01:22 +0530

    Spacer,icon and status changes

--- App.js
+++ App.js
@@ -2,8 +2,24 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { RestaurantsScreen } from "./src/features/restuarants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 
 export default function App() {
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  if (!latoLoaded || !oswaldLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/features/restuarants/components/restaurant.info.card.component.js
+++ src/features/restuarants/components/restaurant.info.card.component.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+import { View, Image, Text } from "react-native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "./spacer/spacer.component";
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.text.primary};
 `;
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.text.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
@@ -14,23 +30,67 @@ const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Ratings = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const OpenSVG = styled(SvgXml)`
+  flex-direction: row;
+`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
     ],
     address = "Some address 100",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Ratings>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Ratings>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant={"left.large"} />
+            {isOpenNow && <OpenSVG xml={open} width={20} height={20} />}
+            <Spacer variant={"left.large"} />
+            <Image style={{ width: 20, height: 20 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
